cdd4c0045c66bfecbf279ecaacf656d5bbbd4c2c
First Repo <ejaz.hassaan93@outlook.com>
2024-02-25 00:42:28 +0500

    new

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log(js attached file)
+console.log('js attached file')
+<script>
+   <><div class="my Class">This is the first element with class 'myClass'</div>
+    <div class="my Class">This is the second element with class 'myClass'</div><script>
+// Retrieve the element with class 'myClass'
+        var class = document.querySelector('.myClass');
+
+        // Log the element to the console
+        console.log(myClass);
+    </script></>
